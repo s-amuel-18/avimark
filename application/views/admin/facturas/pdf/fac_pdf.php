afc72fbfd00402159b6597cf72e5b4a96e5b9605
Samuel Graterol <samuelgraterol12@gmail.com>
2022-03-07 09:18:42 -0400

    se cambio DATA por CLIENT en la factura.

--- application/views/admin/facturas/pdf/fac_pdf.php
+++ application/views/admin/facturas/pdf/fac_pdf.php
@@ -102,9 +102,9 @@ $fechas_actual = date_format($date, "d/m/Y");
 
             <td class="w-50">
               <div style="font-size: 18px;" class="title-3 text-orange text-right">
-                CLIENT
+                DATA
               </div>
-              <p class="m-0 text-right"><strong>DATA</strong>: <?php echo $factura->nombre_cliente ?></p>
+              <p class="m-0 text-right"><strong>CLIENT</strong>: <?php echo $factura->nombre_cliente ?></p>
               <p class="m-0 text-right"><strong>PAYMENT TERMS</strong>: Payment for <?php echo $factura->nombre_cartera ?></p>
               <p class="m-0 text-right"><strong>to the email</strong>: <?php echo $factura->email_cartera ?></p>
             </td>
