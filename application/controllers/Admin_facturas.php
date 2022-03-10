a9dc367a726cf8c583e767fe7eff9911766e748f
Samuel Graterol <samuelgraterol12@gmail.com>
2022-03-10 11:45:31 -0400

    Se agregaron los servicios en el controlador de la vista de facturas

--- application/controllers/Admin_facturas.php
+++ application/controllers/Admin_facturas.php
@@ -383,6 +383,9 @@ class Admin_facturas  extends CI_Controller
     $categorias = $this->categoria_model->get_all();
     $data["categorias"] = $categorias;
 
+		$servicios = $this->db->select(["id", "nombre", "precio_total"])->get("servicios")->result();
+		$data["servicios"] = $servicios;
+
     $clientes = $this->db->get("clientes")->result();
     $data["clientes"] = $clientes;
 
