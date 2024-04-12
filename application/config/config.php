96f61ff5e28c919d46b189b5447bd60a50482862
s-amuel-18 <63316336+s-amuel-18@users.noreply.github.com>
2024-04-12 15:25:21 -0400

    Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/avimark';
-
+$config['base_url'] = 'https://avimarkstudio.com/avimark-studio/';
 
 /*
 |--------------------------------------------------------------------------
